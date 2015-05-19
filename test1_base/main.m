f5c5b40db00bb2dd774a2a7af94ba4d938d0ca71
manana <manana@baroja24.local>
2015-05-19 11:03:17 +0200

    Initial Commit

---
+++ test1_base/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test1_base
+//
+//  Created by manana on 19/05/15.
+//  Copyright (c) 2015 ios8. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
